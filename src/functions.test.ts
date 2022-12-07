83ddc2d6924f3071a7d1056c640ae07ab176ed19
Sarah Lancaster <sarah.lancaster@springload.co.nz>
2022-12-08 12:05:57 +1300

    Add passing test

--- src/functions.test.ts
+++ src/functions.test.ts
@@ -1,5 +1,42 @@
-import { addFive } from "./functions";
+import { convertDataPeopleToSearchString, isFive } from "./functions";
 
-test("adds 5 to a number", () => {
-  expect(addFive(2)).toBe(7);
+test("5 is 5", () => {
+  expect(isFive(5)).toBeTruthy();
+});
+test("4 is not 5", () => {
+  expect(isFive(4)).toBeFalsy();
+});
+
+test("it creates a string for indexing people", () => {
+  const people = [
+    {
+      people: [
+        {
+          name: "Jane Doe",
+          role: "Co-ordinator",
+        },
+      ],
+      title: "Northland",
+    },
+    {
+      people: [
+        {
+          name: "Jane Doe",
+          role: "Director",
+        },
+        {
+          name: "Johnathan Doe",
+          role: "Director",
+        },
+        {
+          name: "Jimin Park",
+          role: "Main dancer",
+        },
+      ],
+      title: "Auckland",
+    },
+  ];
+  const expected =
+    "Northland: Jane Doe - Co-ordinator, Auckland: Jane Doe - Director, Johnathan Doe - Director, Jimin Park - Main dancer";
+  expect(convertDataPeopleToSearchString(people)).toEqual(expected);
 });
--- src/functions.ts
+++ src/functions.ts
@@ -1,1 +1,26 @@
 export const addFive = (number: number): number => number + 5;
+
+export const subtractFive = (number: number): number => number - 5;
+
+export const isFive = (number: number): boolean => number === 5;
+
+type DataPeople = { name: string; role: string };
+export type DataPeopleSection = {
+  title: string;
+  people: DataPeople[];
+};
+export const convertDataPeopleToSearchString = (
+  people: DataPeopleSection[]
+): string => {
+  const peopleContent = people.map((p) => {
+    const title = `${p.title}:`;
+    const peopleStrings = p.people.map((q, index) => {
+      const isSolo = p.people.length === 1;
+      const isFormatted = !isSolo && index !== p.people.length - 1;
+      return `${q.name} - ${q.role}${isFormatted ? "," : ""}`;
+    });
+    const content = [title, ...peopleStrings];
+    return content.join(" ");
+  });
+  return peopleContent.join(", ");
+};
